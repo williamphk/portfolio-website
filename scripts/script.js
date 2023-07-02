5d364dc63eba3d40a207d8fa59cf7f9fd73f8373
William Poon <willphk@outlook.com>
2023-07-02 19:08:11 -0400

    Fixed unintersecting bug

--- scripts/script.js
+++ scripts/script.js
@@ -8,20 +8,38 @@
 // }
 
 // Scroll Animation
+let intersectingImageArray = [];
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
+    const projectWrapper =
+      entry.target.parentElement.parentElement.parentElement.parentElement
+        .parentElement;
+    const imageWrapper = projectWrapper.querySelector(".image-wrapper");
+    const imageElement = imageWrapper.querySelector("img");
+    const imageUrl = entry.target.getAttribute("data-img");
+
     if (entry.isIntersecting) {
-      const projectWrapper =
-        entry.target.parentElement.parentElement.parentElement;
-      const imageWrapper = projectWrapper.querySelector(".image-wrapper");
-      console.log(imageWrapper);
-      const imageElement = imageWrapper.querySelector("img");
-      const imageUrl = entry.target.getAttribute("data-img");
+      //console.log("Intersected");
+      if (!intersectingImageArray.includes(imageUrl)) {
+        intersectingImageArray.push(imageUrl);
+      }
       imageElement.src = imageUrl;
+      //console.log(intersectingImageArray);
+    } else {
+      //console.log("Unintersected");
+      const index = intersectingImageArray.indexOf(imageUrl);
+      if (index > -1) {
+        intersectingImageArray.splice(index, 1);
+      }
+      if (intersectingImageArray.length > 0) {
+        imageElement.src =
+          intersectingImageArray[intersectingImageArray.length - 1];
+      }
+      //console.log(intersectingImageArray);
     }
   });
 });
 
-const observeElements = document.querySelectorAll(".project-introduction");
+const observeElements = document.querySelectorAll(".intersecting-element");
 observeElements.forEach((el) => observer.observe(el));
